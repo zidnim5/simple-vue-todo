25e062431ed4cbb5bc8dfd2b6495cd4f896687b5
= <=>
2020-08-30 11:11:41 +0700

    update function and done function

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -8,12 +8,17 @@
   <div class="todo-list-box">
     <p class="todo-list-title">List Todo :</p>
     <div class="todo-list-job" v-for="(todoList, index) in todoLists" :key="todoList.id">
-      <div>
+      <input type="checkbox" v-model="todoList.completed">
+      <div class="todo-job" :class="{todo_completed: todoList.completed}" v-if="!todoList.editing" @dblclick="editMode(todoList)">
         {{todoList.title}}
       </div>
+      <input v-focus v-else type="text" class="input-edit" @blur="doneEdit(todoList)"  @keyup.enter="doneEdit(todoList)" @keyup.esc="cancelEdit(todoList)" v-model="todoList.title">
       <div @click="removeTodo(index)" class="todo-remove">
         &times;
       </div>
+    </div>
+    <div v-if="todoLists.length < 1" class="enjoy">
+      Nothing job today
     </div>
   </div>
 </template>
@@ -33,14 +38,25 @@ export default {
           {
             id: 1,
             title: 'Going Cook',
+            temporary: '',
             completed: false,
+            editing: false,
           },
           {
             id: 2,
             title: 'Going Wash',
+            temporary: '',
             completed: false,
+            editing: false,
           }
         ],
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
     }
   },
   methods: {
@@ -52,9 +68,24 @@ export default {
       });
     },
 
+    editMode(data) {
+      data.editing = true;
+      data.temporary = data.title;
+    },
+
+    doneEdit(data) {
+      data.editing = false;
+    },
+
+    cancelEdit(data) {
+      data.title = data.temporary;
+      data.editing = false;
+    },
+
     removeTodo(idx) {
       this.todoLists.splice(idx, 1);
     }
+
   }
 }
 </script>
@@ -68,6 +99,14 @@ export default {
   color: rgb(82, 81, 81);
 
   &:focus{
+    outline: 0;
+  }
+}
+.input-edit {
+  padding: 5px 10px;
+  width: 80%;
+  font-size: 16px;
+  &:focus {
     outline: 0;
   }
 }
@@ -91,11 +130,23 @@ export default {
   justify-content: space-between;
   padding: 10px;
 }
+.todo-job {
+  width: 80%;
+}
 .todo-remove {
   &:hover {
     color: rgb(240, 68, 0);
     cursor: pointer;
   }
+}
+.todo_completed {
+  text-decoration: line-through;
+}
+.enjoy {
+  padding: 20px 0;
+  text-align: center;
+  font-size: 40px;
+  color:rgb(160, 160, 160);
 }
 h3 {
   margin: 40px 0 0;
